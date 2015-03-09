c96ca52ad8f5891c042bfb7a9a7550ebfbb84b75
Jesse Copeland <jesse.copeland@gmail.com>
2015-03-09 12:02:17 -1000

    api rourtes started and tested, working

---
+++ server/controllers/api.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+
+// list all todos
+router.get('/', function (req, res) {
+  res.send('Hello API');
+});
+
+// add todo
+
+
+// delete todo
+
+
+// completed todo
+
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var app = express();
+
+//DB Connection
+mongoose.connect('mongodb://localhost/angular_todo');
+
+// Middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
+// Routes
+app.use('/api', require('./controllers/api'));
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
